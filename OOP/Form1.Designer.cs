c7af91013cff4460c9e33cd8658fa2ac7e74215c
nunuzy <fnukhi@gmail.com>
2021-11-14 14:14:59 +0700

    crud oop

---
+++ OOP/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtStok = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtkode = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtharga = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnama = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtjenis = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.datagridview = new System.Windows.Forms.DataGridView();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.btnrefresh = new System.Windows.Forms.Button();
+            this.btntambah = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.txtdabar = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.txtStok);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtkode);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtharga);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtnama);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtjenis);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(668, 276);
+            this.panel1.TabIndex = 43;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Teal;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(199, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(68, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 31;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Teal;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(273, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 37);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "BETAMART";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Teal;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(668, 79);
+            this.panel2.TabIndex = 32;
+            // 
+            // txtStok
+            // 
+            this.txtStok.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStok.Location = new System.Drawing.Point(297, 229);
+            this.txtStok.Name = "txtStok";
+            this.txtStok.Size = new System.Drawing.Size(228, 27);
+            this.txtStok.TabIndex = 29;
+            this.txtStok.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(150, 233);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 18);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Stok";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtkode
+            // 
+            this.txtkode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtkode.Location = new System.Drawing.Point(297, 96);
+            this.txtkode.Name = "txtkode";
+            this.txtkode.Size = new System.Drawing.Size(228, 27);
+            this.txtkode.TabIndex = 0;
+            this.txtkode.TextChanged += new System.EventHandler(this.txtkode_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(150, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 18);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Kode Barang";
+            // 
+            // txtharga
+            // 
+            this.txtharga.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtharga.Location = new System.Drawing.Point(297, 162);
+            this.txtharga.Name = "txtharga";
+            this.txtharga.Size = new System.Drawing.Size(228, 27);
+            this.txtharga.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(150, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 18);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Harga";
+            // 
+            // txtnama
+            // 
+            this.txtnama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnama.Location = new System.Drawing.Point(297, 129);
+            this.txtnama.Name = "txtnama";
+            this.txtnama.Size = new System.Drawing.Size(228, 27);
+            this.txtnama.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(150, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 18);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Nama Barang";
+            // 
+            // txtjenis
+            // 
+            this.txtjenis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtjenis.FormattingEnabled = true;
+            this.txtjenis.Items.AddRange(new object[] {
+            "Makanan",
+            "Minuman",
+            "Kosmetik",
+            "Electronik"});
+            this.txtjenis.Location = new System.Drawing.Point(297, 195);
+            this.txtjenis.Name = "txtjenis";
+            this.txtjenis.Size = new System.Drawing.Size(228, 28);
+            this.txtjenis.TabIndex = 3;
+            this.txtjenis.SelectedIndexChanged += new System.EventHandler(this.txtjenis_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(150, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 18);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Jenis";
+            // 
+            // datagridview
+            // 
+            this.datagridview.BackgroundColor = System.Drawing.Color.White;
+            this.datagridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagridview.Location = new System.Drawing.Point(12, 298);
+            this.datagridview.Name = "datagridview";
+            this.datagridview.Size = new System.Drawing.Size(643, 192);
+            this.datagridview.TabIndex = 42;
+            this.datagridview.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datagridview_CellContentClick);
+            // 
+            // btnexit
+            // 
+            this.btnexit.BackColor = System.Drawing.Color.Crimson;
+            this.btnexit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexit.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnexit.Location = new System.Drawing.Point(564, 496);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(91, 37);
+            this.btnexit.TabIndex = 40;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = false;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // btnrefresh
+            // 
+            this.btnrefresh.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnrefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnrefresh.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnrefresh.Location = new System.Drawing.Point(467, 497);
+            this.btnrefresh.Name = "btnrefresh";
+            this.btnrefresh.Size = new System.Drawing.Size(91, 37);
+            this.btnrefresh.TabIndex = 39;
+            this.btnrefresh.Text = "Refresh";
+            this.btnrefresh.UseVisualStyleBackColor = false;
+            this.btnrefresh.Click += new System.EventHandler(this.btnrefresh_Click);
+            // 
+            // btntambah
+            // 
+            this.btntambah.BackColor = System.Drawing.Color.SteelBlue;
+            this.btntambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntambah.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btntambah.Location = new System.Drawing.Point(12, 497);
+            this.btntambah.Name = "btntambah";
+            this.btntambah.Size = new System.Drawing.Size(91, 37);
+            this.btntambah.TabIndex = 35;
+            this.btntambah.Text = "Create";
+            this.btntambah.UseVisualStyleBackColor = false;
+            this.btntambah.Click += new System.EventHandler(this.btntambah_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.BackColor = System.Drawing.Color.SteelBlue;
+            this.btndelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btndelete.Location = new System.Drawing.Point(216, 497);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(91, 37);
+            this.btndelete.TabIndex = 37;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = false;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnupdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnupdate.Location = new System.Drawing.Point(114, 497);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(91, 37);
+            this.btnupdate.TabIndex = 36;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = false;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // txtdabar
+            // 
+            this.txtdabar.AutoSize = true;
+            this.txtdabar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdabar.ForeColor = System.Drawing.Color.White;
+            this.txtdabar.Location = new System.Drawing.Point(12, 279);
+            this.txtdabar.Name = "txtdabar";
+            this.txtdabar.Size = new System.Drawing.Size(92, 16);
+            this.txtdabar.TabIndex = 33;
+            this.txtdabar.Text = "Data Barang";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(668, 546);
+            this.Controls.Add(this.txtdabar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.datagridview);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btnrefresh);
+            this.Controls.Add(this.btntambah);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Betamart";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtkode;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtharga;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnama;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox txtjenis;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView datagridview;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Button btnrefresh;
+        private System.Windows.Forms.Button btntambah;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStok;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label txtdabar;
+    }
+}
+
---
+++ OOP/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace OOP
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Bootcamp\OOP\dboop.accdb");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btntambah_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                OleDbCommand cmd = new OleDbCommand("Insert into tb_barang (kode_barang,nama_barang,harga,jenis,stok) values('" + txtkode.Text + "', '" + txtnama.Text + "', '" + txtharga.Text + "', '" + txtjenis.Text + "', '"+txtStok.Text+ "')", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Data Berhasil Ditambahkan");
+                cmd.Dispose();
+
+                cleartext();
+                fillgrid();
+            }
+            catch (Exception)
+            { 
+                MessageBox.Show("Data Gagal Ditambahkan");
+            }
+            
+        }
+
+        void fillgrid()
+        {
+            con.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter("select * from tb_barang order by ID", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            datagridview.DataSource = dt;
+            con.Close();
+        }
+
+        void cleartext()
+        {
+            txtkode.Text = "";
+            txtnama.Text = "";
+            txtharga.Text = "";
+            txtjenis.Text = "";
+            txtStok.Text = "";
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                con.Open();
+                OleDbCommand cmd = new OleDbCommand("Update tb_barang set kode_barang='" + txtkode.Text + "', nama_barang='" + txtnama.Text + "', harga='" + txtharga.Text + "', jenis='" + txtjenis.Text + "', stok='"+txtStok.Text+ "' where kode_barang = '" + txtkode.Text + "' ", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Data Berhasil Diupdate");
+                cmd.Dispose();
+
+                cleartext();
+                fillgrid();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Data Gagal Diupdatet");
+            }
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                OleDbCommand cmd = new OleDbCommand("Delete from tb_barang where kode_barang = '" + txtkode.Text + "' ", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Data Berhasil Didelete");
+                cmd.Dispose();
+
+                cleartext();
+                fillgrid();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Data Gagal Didelete");
+            }
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnrefresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "select * from tb_barang";
+                OleDbCommand cmd= new OleDbCommand(query, con);
+                DataSet ds = new DataSet();
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                adapter.Fill(ds, "res");
+                datagridview.DataSource = ds.Tables["res"];
+                adapter.Dispose();
+                cmd.Dispose();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Gagal Menampilkan Data");
+            }
+        }
+
+        private void txtkode_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtsearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtjenis_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void datagridview_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
